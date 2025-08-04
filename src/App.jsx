1e6fbf6a56b85d3ecd55b638e3b6f5da72acc688
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-08-04 17:54:29 +0300

    feat: setup state management for city, data, loading and error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const API_KEY = "65e41c64cba3cc5ba65aa74b742dcb34";
+
   return (
     <div className="weather-app">
       <header>
         <h1>Application Meteo</h1>
         <form>
-          <input type="text" placeholder="Entrez le nom d'une ville" />
+          <input type="text" placeholder="Entrez le nom d'une ville" value={city} 
+          onChange={(e) => setCity(e.target.value)}/>
           <button type="submit">Rechercher</button>
         </form>
       </header>
